083e3fc7935c68a84039a7bc411071ac0e54dae2
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-14 17:10:51 -0400

    4/4 tests completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,30 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+
+    index = 0
+    room_list = []
+    while index < len(names):
+        room_list.append(f"Hello, {names[index]}! You'll be assigned to room {index + 1}!")
+        index += 1
+    
+    return room_list
+   
 
 def printer(names):
-    return None
+
+    print_badges = batch_badge_creator(names)
+    for badge in print_badges:
+        print(badge)
+
+    print_room_assignment = assign_rooms(names)
+    for badge in print_room_assignment:
+        print(badge)
+   
+
+
+printer(["Arel", "Carol"])
